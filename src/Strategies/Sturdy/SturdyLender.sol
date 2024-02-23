f0158c36fd08a16602667ac392c74ad8d9076eee
Schlagonia <schlagonia@gmail.com>
2024-02-23 15:53:56 -0700

    build: with auction swapper

--- src/Strategies/Sturdy/SturdyLender.sol
+++ src/Strategies/Sturdy/SturdyLender.sol
@@ -1,16 +1,24 @@
 // SPDX-License-Identifier: AGPL-3.0
 pragma solidity 0.8.18;
 
-// We use the Tokenized Strategy interface.
-import {Base4626, ERC20} from "../../Base4626.sol";
-import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {Base4626} from "../../Base4626.sol";
+import {AuctionSwapper} from "@periphery/swappers/AuctionSwapper.sol";
 
-contract SturdyLender is Base4626 {
-    using SafeERC20 for ERC20;
-
+contract SturdyLender is Base4626, AuctionSwapper {
     constructor(
         address _asset,
         string memory _name,
         address _vault
     ) Base4626(_asset, _name, _vault) {}
+
+    function setAuction(address _auction) external onlyEmergencyAuthorized {
+        auction = _auction;
+    }
+
+    function _auctionKicked(
+        address _token
+    ) internal virtual override returns (uint256) {
+        require(_token != address(asset), "asset");
+        return super._auctionKicked(_token);
+    }
 }
